8d532145bebc88fac722c9afcb6bf4043d400280
Sanjid Rahman <sanjid25@gmail.com>
2021-05-03 15:29:27 +1200

    shoot warning before generating token

--- R/Token.R
+++ R/Token.R
@@ -6,6 +6,9 @@
   realm <- Sys.getenv("SHAREPOINT_REALM") # also directory_id
   #principal <- "00000003-0000-0ff1-ce00-000000000000"
   #target <- "opdepot.sharepoint.com"
+
+  if(!length(clientid)) warning("Client ID not found! Please follow instructions in README file.")
+
   access_url <- sprintf("https://login.microsoftonline.com/%s/oauth2/v2.0/token", realm)
   access_token <- httr::content(httr::POST(url = access_url, body = list(
     grant_type = "client_credentials", client_id = clientid, client_secret = clientsecret,
@@ -13,6 +16,7 @@
   Sys.setenv(SHAREPOINT_TOKEN = sprintf("Bearer %s", access_token))
 
   if(!length(access_token)) warning("Could not generate access token. Please follow instructions in README file.")
+
 
   # message("Sharepoint token set to system environment variable as `SHAREPOINT_TOKEN` ")
   # return(sprintf("Bearer %s", access_token))
